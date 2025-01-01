5892dd6e832418a1e990fd94c0abb65c48456bfc
Karamshaat2023 <121696455+Karamshaat2023@users.noreply.github.com>
2025-01-01 14:07:00 +0200

    This is My  First viersion appliction vi.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,6 +6,10 @@ android {
     namespace = "com.example.estateapp"
     compileSdk = 34
 
+    buildFeatures {
+        viewBinding  = true
+    }
+
     defaultConfig {
         applicationId = "com.example.estateapp"
         minSdk = 24
